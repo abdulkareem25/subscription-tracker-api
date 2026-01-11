7e221f391d09aab835d0a4c5891a23fad7cd0311
Abdul Kareem <abdulkareemzahir9599@gmail.com>
2026-01-11 23:14:23 +0530

    required dependencies installed, and database connected

---
+++ app.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import { PORT } from './config/env.js';
+import connectToDb from './config/db.js';
+
+import authRouter from './routes/auth.routes.js';
+import userRouter from './routes/user.routes.js';
+import subcriptionRouter from './routes/subscription.routes.js';
+
+
+const app = express();
+
+app.use('/api/v1/auth', authRouter)
+app.use('/api/v1/users', userRouter)
+app.use('/api/v1/subcriptions', subcriptionRouter)
+
+
+app.get('/', (req, res) => {
+    res.send('Welcome to my first Backend Project')
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`)
+    connectToDb()
+})
+
+
+export default app;
---
+++ config/db.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import { MONGO_URI, NODE_ENV } from "./env.js";
+
+if(!MONGO_URI){
+    throw new Error("MONGO_URI is not defined in the environment variables");
+}
+
+const connectToDb = async () => {
+    try {
+        await mongoose.connect(MONGO_URI);
+        console.log(`Connected to MongoDB in ${NODE_ENV}mode`);
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+        throw error; 
+    }
+}
+
+export default connectToDb;
